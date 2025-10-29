51799523073f53fb52546b9f20e5a8b4ee3d6d38
kimchaeryn <ryn3439@naver.com>
2025-10-29 11:53:20 +0900

    제목을 추가함

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>제목을 최종 수정함</h1>
+      <h2>제목을 추가함</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
