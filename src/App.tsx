93a527171d1395353b49d87036f252306b07cc47
kimchaeryn <ryn3439@naver.com>
2025-10-29 11:51:15 +0900

    key 이름을 잘못 써서 다시 생성 후 제목 최종 수정함

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>제목을 수정함</h1>
+      <h1>제목을 최종 수정함</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
